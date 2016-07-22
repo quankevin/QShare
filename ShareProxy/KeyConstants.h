6bfb1cbc27ea5d43251a5e315000a6e46332fcfc
quankevin <quankevin@163.com>
2016-07-22 09:31:31 +0800

    去掉一些key

--- ShareProxy/KeyConstants.h
+++ ShareProxy/KeyConstants.h
@@ -1,30 +1,22 @@
-//
-//  KeyConstants.h
-//  zhuazhua
-//
-//  Created by bu88 on 15/4/23.
-//  Copyright (c) 2015年 guohao. All rights reserved.
-//
-
 #ifndef KeyConstants_h
 #define KeyConstants_h
 
-#define sinaWBKey           @"wb2267783501"
-#define sinaAppKey          @"2267783501"
-#define sinaRedirectURI     @"http://www.wn517.com/"
+#define sinaWBKey           @"xxx"
+#define sinaAppKey          @"xxx"
+#define sinaRedirectURI     @"xxx"
 
-#define qqAppKey            @"1104759031"
+#define qqAppKey            @"xxx"
 
-#define wxAppKey            @"wxa41e061dee0cad3b"
-#define wxAppSecret         @"1cfc7a08c0abfb899ea1e0ad9583d270"
+#define wxAppKey            @"xxx"
+#define wxAppSecret         @"xxx"
 
-#define rongKeyDev          @"kj7swf8o7r4b2"
-#define rongKeyRelease      @"qf3d5gbj3kpuh"
+#define rongKeyDev          @"xxx"
+#define rongKeyRelease      @"xxx"
 
-#define buglyAppId          @"900017033"
+#define buglyAppId          @"xxxx"
 
-#define WnAppId             @"86192ad29b9077f7ac15f08af3c51e6d"
+#define WnAppId             @"xxx"
 
 
-#define alischeme           @"weinong517"
+#define alischeme           @"xxx"
 #endif
